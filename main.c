9d0872cfee103e78e9a45044354bb29d3583208f
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-21 15:26:32 +0100

    style: setup monty

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * main - entry point of the program
+ *
+ * @ac: number of provided arguments
+ * @av: arguments in 2d array format
+ * Return: EXIT_SUCCESS OR EXIT_FAILURE
+ */
+int main(int ac, char **av)
+{
+	printf("Monty Progam Setup!\n");
+	return (EXIT_SUCCESS);
+}
